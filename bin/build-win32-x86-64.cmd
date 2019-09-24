50fb0fd8eb8551db28bb69f0102fd65f9271579c
Bogdan Popa <bogdan@defn.io>
2019-09-24 15:31:18 +0300

    win32: fix platform for x86-64

--- bin/build-win32-x86-64.cmd
+++ bin/build-win32-x86-64.cmd
@@ -4,6 +4,6 @@ git submodule update --init
 mkdir artifacts\win32-x86-64
 cd libsass
 git reset --hard HEAD
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSbuild.exe" win\libsass.sln /p:Configuration=Release
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSbuild.exe" win\libsass.sln /p:Configuration=Release /p:Platform=Win64
 copy win\bin\libsass.dll ..\artifacts\win32-x86-64\libsass.dll
 cd ..
