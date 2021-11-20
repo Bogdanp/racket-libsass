804a84450967c6a0989c651d068cbda8bcf7b85c
Bogdan Popa <bogdan@defn.io>
2021-11-20 15:03:17 +0200

    build: fix version numbers

--- libsass-x86_64-linux/info.rkt
+++ libsass-x86_64-linux/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 
-(define version "3.6.1")
+(define version "3.6.5")
 (define deps '("base"))
 (define copy-foreign-libs '("libsass.so"))
 (define install-platform #rx"x86_64-linux")
--- libsass-x86_64-win32/info.rkt
+++ libsass-x86_64-win32/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 
-(define version "3.6.1")
+(define version "3.6.5")
 (define deps '("base"))
 (define copy-foreign-libs '("libsass.dll"))
 (define install-platform #rx"win32.x86_64")
