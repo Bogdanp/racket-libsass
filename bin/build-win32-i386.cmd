cc3037016d266c2c9f10f498f7e5e6aa0697ce32
Bogdan Popa <bogdan@defn.io>
2022-12-18 11:29:59 +0200

    build: fixup MSBuild path for windows-2019 change

--- bin/build-win32-i386.cmd
+++ bin/build-win32-i386.cmd
@@ -3,6 +3,6 @@
 git submodule update --init
 mkdir artifacts\win32-i386
 cd libsass
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\15.0\Bin\MSbuild.exe" win\libsass.sln /p:Configuration=Release /p:Platform=Win32 /m
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSbuild.exe" win\libsass.sln /p:Configuration=Release /p:Platform=Win32 /m
 copy win\bin\libsass.dll ..\artifacts\win32-i386\libsass.dll
 cd ..
--- bin/build-win32-x86-64.cmd
+++ bin/build-win32-x86-64.cmd
@@ -3,6 +3,6 @@
 git submodule update --init
 mkdir artifacts\win32-x86-64
 cd libsass
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\15.0\Bin\MSbuild.exe" win\libsass.sln /p:Configuration=Release /p:Platform=Win64 /m
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSbuild.exe" win\libsass.sln /p:Configuration=Release /p:Platform=Win64 /m
 copy win\bin\libsass.dll ..\artifacts\win32-x86-64\libsass.dll
 cd ..
