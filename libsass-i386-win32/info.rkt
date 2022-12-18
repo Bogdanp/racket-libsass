cd62519891cf577f31890f07a74e1312c3e82e2c
Bogdan Popa <bogdan@defn.io>
2022-12-18 11:33:25 +0200

    build: bump versions for new release

--- libsass-i386-win32/info.rkt
+++ libsass-i386-win32/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define license 'BSD-3-Clause)
-(define version "3.6.5")
+(define version "3.6.5.2")
 (define deps '("base"))
 (define copy-foreign-libs '("libsass.dll"))
 (define install-platform #rx"win32.i386")
--- libsass-x86_64-macosx/info.rkt
+++ libsass-x86_64-macosx/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define license 'BSD-3-Clause)
-(define version "3.6.5")
+(define version "3.6.5.2")
 (define deps '("base"))
 (define copy-foreign-libs '("libsass.dylib"))
 (define install-platform #rx"x86_64-macosx")
